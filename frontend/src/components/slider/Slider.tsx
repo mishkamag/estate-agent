52e668b7d5af03b6941531f22c6a614bfbc76e64
mishkamag <m93.maglaperidze@gmail.com>
2024-05-29 02:35:46 +0400

    feat: add slider but without any package

--- frontend/src/components/slider/Slider.tsx
+++ frontend/src/components/slider/Slider.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./slider.scss";
 
 interface SliderProps {
@@ -5,15 +6,60 @@ interface SliderProps {
 }
 
 const Slider = ({ images }: SliderProps) => {
-  console.log(images);
+  const [imageIndex, setImageIndex] = useState<null | number>(null);
+
+  const changeSlide = (direction: "left" | "right") => {
+    setImageIndex((prevIndex) => {
+      if (prevIndex === null) {
+        return direction === "right" ? 1 : images.length - 1;
+      }
+
+      if (direction === "right") {
+        return prevIndex === images.length - 1 ? 0 : prevIndex + 1;
+      }
+
+      if (direction === "left") {
+        return prevIndex === 0 ? images.length - 1 : prevIndex - 1;
+      }
+
+      return prevIndex;
+    });
+  };
+
   return (
     <section className="slider">
+      {imageIndex !== null && (
+        <div className="fullScreenSlider">
+          <div className="arrow" onClick={() => changeSlide("left")}>
+            <img src="/arrow.png" alt="" />
+          </div>
+          <div className="imageContainer">
+            <img src={images[imageIndex]} alt="" />
+          </div>
+          <div className="arrow" onClick={() => changeSlide("right")}>
+            <img
+              src="/arrow.png"
+              style={{ transform: "rotate(180deg)" }}
+              alt=""
+            />
+          </div>
+          <div className="close" onClick={() => setImageIndex(null)}>
+            x
+          </div>
+        </div>
+      )}
+
       <div className="bgImage">
-        <img src={images[0]} alt="" />
+        <img src={images[0]} alt="" onClick={() => setImageIndex(0)} />
       </div>
       <div className="smallImages">
         {images.slice(1).map((image: string, i: number) => (
-          <img key={i} src={image} alt="" />
+          <img
+            key={i}
+            src={image}
+            alt=""
+            onClick={() => setImageIndex(i + 1)}
+          />
         ))}
       </div>
     </section>

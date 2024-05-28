fa4e01fc857814bbf524a2a009504ad4b763848d
mishkamag <m93.maglaperidze@gmail.com>
2024-05-29 00:28:03 +0400

    feat: add slider image feature

--- frontend/src/components/slider/Slider.tsx
+++ frontend/src/components/slider/Slider.tsx
@@ -1,5 +1,23 @@
-const Slider = () => {
-  return <div>Slider</div>;
+import "./slider.scss";
+
+interface SliderProps {
+  images: string[];
+}
+
+const Slider = ({ images }: SliderProps) => {
+  console.log(images);
+  return (
+    <section className="slider">
+      <div className="bgImage">
+        <img src={images[0]} alt="" />
+      </div>
+      <div className="smallImages">
+        {images.slice(1).map((image: string, i: number) => (
+          <img key={i} src={image} alt="" />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Slider;

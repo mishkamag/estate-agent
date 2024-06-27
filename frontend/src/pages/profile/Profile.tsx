fffabeacb8afc1aa5b3acbf654b806520cdc3feb
mishkamag <m93.maglaperidze@gmail.com>
2024-06-28 00:37:07 +0400

    feat: add profile logout functionality

--- frontend/src/pages/profile/Profile.tsx
+++ frontend/src/pages/profile/Profile.tsx
@@ -1,8 +1,26 @@
+import axios from "axios";
 import Chat from "../../components/chat/Chat";
 import List from "../../components/list/List";
 import "./profile.scss";
+import { useNavigate } from "react-router-dom";
 
 const Profile = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/api/auth/logout",
+        {},
+        { withCredentials: true }
+      );
+      localStorage.removeItem("user");
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="profile">
       <div className="detailsContainer">
@@ -25,6 +43,7 @@ const Profile = () => {
             <span>
               E-mail: <b>m93@gmail.com</b>{" "}
             </span>
+            <button onClick={handleLogout}>Logout</button>
           </div>
           <div className="title">
             <h1>My List</h1>
